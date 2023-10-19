ab1e41fce4f2d1136d3b4ce5b187fce3435b4622
jeremielabelle <73847901+jeremielabelle@users.noreply.github.com>
2023-10-19 13:15:33 -0400

    Ajout du fichier de control de moteur.

---
+++ Projet3/Core/Inc/controlStepMotor.h
@@ -0,0 +1,14 @@
+/*
+ * controlStepMotor.h
+ *
+ *  Created on: Oct 19, 2023
+ *      Author: DJLAB
+ */
+
+#ifndef INC_CONTROLSTEPMOTOR_H_
+#define INC_CONTROLSTEPMOTOR_H_
+
+#include "controlStepMotor.c"
+// Hi Vincent, here is a new file :)
+
+#endif /* INC_CONTROLSTEPMOTOR_H_ */
